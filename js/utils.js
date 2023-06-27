e305dd1d1320bea33dd52a599131d589c2efa0ca
Niuttt <72707364+Niniutt@users.noreply.github.com>
2023-06-27 13:24:11 +0200

    Jolie page d'accueil
    <3<3<3

---
+++ js/utils.js
@@ -0,0 +1,22 @@
+
+function load() {
+    // Load header.html
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    // Load footer.html
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
