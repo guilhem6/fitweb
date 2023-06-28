0859ad17c5827f39b575f23171b50bdf26eb8a1b
Niuttt <72707364+Niniutt@users.noreply.github.com>
2023-06-28 15:11:55 +0200

    Possibilité de débugger avec des points d'arrêt dans VSC

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Variables
+var connected = false;
+
+function init() {
+    load();
+    //
+
+
+
+    //
+}
+
+function checkOption(){
+    //This will fire on changing of the value of "requests"
+    var dropDown = document.getElementById("requestDropDown");
+    var textBox = document.getElementById("otherBox");
+
+    if(dropDown.value == "other"){
+        textBox.style.visibility = "visible";
+    }
+}
